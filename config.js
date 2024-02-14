a43557345399a61abcea4c034a65bd6e380706c2
maanimis <maani.meisam@gmail.com>
2024-02-15 02:44:00 +0330

    update

---
+++ config.js
@@ -0,0 +1,10 @@
+export const config = {
+  adsCount: 5000,
+  firebaseAPI: "YOUR_API",
+  chromePATH: "/usr/bin/google-chrome-stable",
+  proxy: {
+    enable: true,
+    server: "127.0.0.1:2080",
+  },
+  selectorAds: '[data-testid="placementTracking"]',
+};
